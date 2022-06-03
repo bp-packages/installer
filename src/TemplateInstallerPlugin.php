abd8bfe81c559b9031fda1dbf2a0d85ad0fe7354
leomaciel <dev@leomaciel.com>
2022-06-03 08:17:14 -0300

    ?

--- src/TemplateInstallerPlugin.php
+++ src/TemplateInstallerPlugin.php
@@ -6,7 +6,7 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class TemplateInstallerPlugin implements PluginInterface
+abstract class TemplateInstallerPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
